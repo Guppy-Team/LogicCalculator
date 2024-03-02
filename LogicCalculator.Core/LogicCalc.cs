4ebadf296a7f2bfc8a4ac64060fcbfa15a06c5a8
Bread92 <vaneck.van2019@gmail.com>
2024-03-02 18:56:24 +0300

    Add crude structure

--- LogicCalculator.Core/LogicCalc.cs
+++ LogicCalculator.Core/LogicCalc.cs
@@ -1,5 +1,19 @@
 ﻿namespace LogicCalculator.Core;
 
-public class LogicCalc
+public static class LogicCalc
 {
+    public static void ToDnf(string expression, string function)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public static void ToKnf(string expression, string function)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static bool[,] GetTruthTable(int paramsCount, int functionNumber)
+    {
+        throw new NotImplementedException();
+    }
 }
