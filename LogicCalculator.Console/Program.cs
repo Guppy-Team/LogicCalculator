bbf9ad716192f3ed26a766d743256a9078db0155
k4ffy <schabalov.pasha3@gmail.com>
2024-03-16 10:02:04 +0300

    Add `Cnf`

--- LogicCalculator.Console/Program.cs
+++ LogicCalculator.Console/Program.cs
@@ -1,16 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using LogicCalculator.Core.LogicalExpression;
-using LogicCalculator.Core.Shared.Tokenizers;
-
-var tokenizer = new LogicalTokenizer();
-var tokens = tokenizer.Tokenize("(x1 || !x2 => x3) || x1");
-
-// Создаем экземпляр класса Dnf
-var dnf = new Dnf();
-
-// Вызываем метод Build, передавая список токенов
-var dnfExpression = dnf.Build(tokens);
-
-// Выводим результат
-Console.WriteLine(dnfExpression); // Выведет: (a && b) || (!a && c)
+Console.WriteLine("Hello world!");
