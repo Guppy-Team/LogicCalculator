4590a65e1dc3065d4ac5b3ac814cfabebfb10638
Bread92 <vaneck.van2019@gmail.com>
2024-03-29 21:01:56 +0300

    Add basic Expression structure

---
+++ LogicCalculator.Core/Shared/ExpressionEvaluators/RpnExpression.cs
@@ -0,0 +1,12 @@
+﻿using LogicCalculator.Core.Shared.Interfaces;
+
+namespace LogicCalculator.Core.Shared.ExpressionEvaluators;
+
+record RpnExpression(IToken[] tokens, Dictionary<IToken, Func<double[],double>> func) : IExpression
+{
+    // 3 2 +
+    public double Evaluate((string, double)[] variables)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ LogicCalculator.Core/Shared/Interfaces/IExpression.cs
@@ -0,0 +1,6 @@
+﻿namespace LogicCalculator.Core.Shared.Interfaces;
+
+public interface IExpression
+{
+    public double Evaluate((string, double)[] variables);
+}
